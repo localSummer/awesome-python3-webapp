eaeabd6729573a660281fe017691ed37f8581a87
localSummer <roamingcode@163.com>
2018-02-22 09:37:59 +0800

    update

---
+++ www/handlers.py
@@ -0,0 +1,13 @@
+' url handlers '
+
+import re, time, json, logging, hashlib, base64, asyncio
+from coroweb import get, post
+from models import User, Blog, Comment, next_id
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
